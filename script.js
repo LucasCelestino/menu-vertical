4508d58a4c1cd4d6b307c4623215c43e0499d506
Lucas Celestino <lucasrashido@gmail.com>
2020-05-29 12:39:06 -0300

    script js

---
+++ script.js
@@ -0,0 +1,22 @@
+// function openMenu()
+// {
+//     var menu = document.getElementById("menu");
+//     var content = document.getElementById("content");
+//     var open = menu.getAttribute("open");
+
+//     if(open == "true")
+//     {
+//         menu.style.width = "70px";
+//         content.style.width = "100%";
+//         content.style.marginLeft = "0";
+//         menu.setAttribute("open", "false");
+
+//     }
+//     else
+//     {
+//         menu.style.width = "20%";
+//         content.style.marginLeft = "20%";
+//         menu.setAttribute("open", "true");
+//     }
+
+// }
